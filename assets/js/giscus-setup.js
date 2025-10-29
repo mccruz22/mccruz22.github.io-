324133627e4217f3f57279dafbac137d2e65bf90
mccruz22 <123128369+mccruz22@users.noreply.github.com>
2025-10-29 04:47:26 +0000

    Deploying to gh-pages from @ mccruz22/mccruz22.github.io@b35ff4256daa12c22b9e4fa16452d8ae1cb0ab5b 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "mccruz22/mccruz22.github.io-",
+    "data-repo": "mccruz22/mccruz22.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
